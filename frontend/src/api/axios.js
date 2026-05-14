085da9a352432d3885f49144c807fa5e52af694e
Maiharshhu <hj.harsh440@gmail.com>
2026-05-14 11:40:54 +0530

    path

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 
+const baseUrl = (import.meta.env.VITE_API_URL || 'http://localhost:5001').replace(/\/$/, '');
+
 const api = axios.create({
-    baseURL: `${import.meta.env.VITE_API_URL.replace(/\/$/, '')}/api`
+    baseURL: `${baseUrl}/api`
 })
 
 
