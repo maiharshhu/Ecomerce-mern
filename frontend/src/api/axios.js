6cef58511ea197b343346e3643c86cc6d7b15229
Maiharshhu <hj.harsh440@gmail.com>
2026-05-14 12:01:03 +0530

    added environmental variables

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const baseUrl = (import.meta.env.VITE_API_URL).replace(/\/$/, '');
+const baseUrl = (import.meta.env.VITE_API_URL || 'http://localhost:5001').replace(/\/$/, '');
 
 const api = axios.create({
     baseURL: `${baseUrl}/api`
